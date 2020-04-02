72d298ac608a869aa8c2e7014ba3a670c36081e9
Zacky <scarecrow.gang.duck@gmail.com>
2020-04-03 01:07:20 +0900

    First commit.

---
+++ build.cmd
@@ -0,0 +1,17 @@
+echo "build start."
+
+rmdir /s /q dist
+del requirements.txt
+
+mkdir dist
+xcopy /e src dist
+pipenv lock -r > requirements.txt
+pip install -r requirements.txt -t dist
+del requirements.txt
+
+cd dist
+rmdir /s /q bin
+for /F %%d in ('dir /ad /b /w *dist-info') do rmdir /s /q %%d
+cd ..
+echo "build complete."
+echo "please zip all in dist."
